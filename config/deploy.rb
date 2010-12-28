694dbc948a439628092c96f1523a610f16a468a9
Roger Braun <davinelulinvega@gmail.com>
2010-12-28 19:41:58 +0100

    lots of stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,33 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "NewOpac"
 
-set :scm, :subversion
+set :repository,  "git@github.com:rogerbraun/NewOpac.git"
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "rokuhara.japanologie.kultur.uni-tuebingen.de"                          # Your HTTP server, Apache/etc
+role :app, "rokuhara.japanologie.kultur.uni-tuebingen.de"                          # This may be the same as your `Web` server
+role :db,  "rokuhara.japanologie.kultur.uni-tuebingen.de", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ptions[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/Users/edv/Sites/opac2"
+set :deploy_via, :remote_cache
+set :user, "edv"
+set :use_sudo, false
+
+
+
+namespace :deploy do
+  task :start, :roles => :app  do 
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+   task :stop do ; end
+   task :restart, :roles => :app do
+  
+   run "touch #{current_path}/tmp/restart.txt"
+  end
+end
