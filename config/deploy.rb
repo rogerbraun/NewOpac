d52117eca70a04503376539616c870552c19d608
Roger Braun <davinelulinvega@gmail.com>
2011-05-16 04:41:15 +0200

    fix stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,6 @@ namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-    run "rm -r #{release_path}/public/uploads"
     run "ln -nfs /Users/edv/uploads_from_opac/ #{release_path}/public/uploads"
   end
 
